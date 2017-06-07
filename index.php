420c02532e54e51db3360c080d41f546688a9af3
sydorenkovd <sydorenkovd@gmail.com>
2017-06-07 13:12:52 +0300

    nemo project init

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sydorenkovd
+ * Date: 07.06.17
+ * Time: 11:06
+ */
+echo 'sd';
---
+++ web/app.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sydorenkovd
+ * Date: 07.06.17
+ * Time: 10:45
+ */
+echo 'hello';
