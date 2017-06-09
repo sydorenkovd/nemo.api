7fa133cade583b5c0f95818ab02aeecbc05c6439
sydorenkovd <sydorenkovd@gmail.com>
2017-06-09 17:46:29 +0300

    request object guzzle test

--- app.php
+++ app.php
@@ -68,26 +68,38 @@ echo "<pre>";
 //], \Nemo\Library\Response\Flight\Search\Request\Segments::class);
 //var_dump($req->getSegments());
 
-$uagent = "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.14";
-$url  = "http://ya.ru";
-$ch = curl_init( $url );
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // возвращает веб-страницу
-curl_setopt($ch, CURLOPT_HEADER, 0);           // не возвращает заголовки
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);   // переходит по редиректам
-curl_setopt($ch, CURLOPT_ENCODING, "");        // обрабатывает все кодировки
-curl_setopt($ch, CURLOPT_USERAGENT, $uagent);  // useragent
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); // таймаут соединения
-curl_setopt($ch, CURLOPT_TIMEOUT, 120);        // таймаут ответа
-curl_setopt($ch, CURLOPT_MAXREDIRS, 10);       // останавливаться после 10-ого редиректа
 
-$content = curl_exec( $ch );
-$err     = curl_errno( $ch );
-$errmsg  = curl_error( $ch );
-$header  = curl_getinfo( $ch );
-curl_close( $ch );
 
-$header['errno']   = $err;
-$header['errmsg']  = $errmsg;
-$header['content'] = $content;
- var_dump($header);
+
+
+
+//=====================================
+//$uagent = "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.14";
+//$url  = "http://ya.ru";
+//$ch = curl_init( $url );
+//
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // возвращает веб-страницу
+//curl_setopt($ch, CURLOPT_HEADER, 0);           // не возвращает заголовки
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);   // переходит по редиректам
+//curl_setopt($ch, CURLOPT_ENCODING, "");        // обрабатывает все кодировки
+//curl_setopt($ch, CURLOPT_USERAGENT, $uagent);  // useragent
+//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); // таймаут соединения
+//curl_setopt($ch, CURLOPT_TIMEOUT, 120);        // таймаут ответа
+//curl_setopt($ch, CURLOPT_MAXREDIRS, 10);       // останавливаться после 10-ого редиректа
+//
+//$content = curl_exec( $ch );
+//$err     = curl_errno( $ch );
+//$errmsg  = curl_error( $ch );
+//$header  = curl_getinfo( $ch );
+//curl_close( $ch );
+//
+//$header['errno']   = $err;
+//$header['errmsg']  = $errmsg;
+//$header['content'] = $content;
+// var_dump($header);
+
+//$client = new \GuzzleHttp\Client([
+//    'base_uri' => 'http://httpbin.org',
+//    'timeout'  => 2.0,
+//]);
--- src/Response/Flight/Search/History/Request.php
+++ src/Response/Flight/Search/History/Request.php
@@ -14,4 +14,86 @@ class Request
 
     private $id;
     private $uri;
+    private $segments;
+    private $passengers;
+    private $parameters;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @param mixed $uri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSegments()
+    {
+        return $this->segments;
+    }
+
+    /**
+     * @param mixed $segments
+     */
+    public function setSegments($segments)
+    {
+        $this->segments = $segments;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassengers()
+    {
+        return $this->passengers;
+    }
+
+    /**
+     * @param mixed $passengers
+     */
+    public function setPassengers($passengers)
+    {
+        $this->passengers = $passengers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param mixed $parameters
+     */
+    public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+    }
 }
